1ee1cbba4f1fb650efdb2d2c56f51595cc058af9
Jörg <ronline1@jihlefeld.de>
2026-06-08 13:01:46 +0200

    ADD AC2400PLUS
    ADD AC2400PLUS

--- zendure_ha_1-2-5-hack/api.py
+++ zendure_ha_1-2-5-hack/api.py
@@ -40,7 +40,7 @@ from .devices.hyper2000 import Hyper2000
 from .devices.solarflow800 import SolarFlow800
 from .devices.solarflow800Plus import SolarFlow800Plus
 from .devices.solarflow800Pro import SolarFlow800Pro
-from .devices.solarflow2400ac import SolarFlow2400AC
+from .devices.solarflow2400ac import SolarFlow2400AC, SolarFlow2400AC_Plus
 from .devices.solarflow2400pro import SolarFlow2400Pro
 from .devices.superbasev4600 import SuperBaseV4600
 from .devices.superbasev6400 import SuperBaseV6400
@@ -68,6 +68,7 @@ class Api:
         "solarflow 800 pro": SolarFlow800Pro,
         "solarflow 800 plus": SolarFlow800Plus,
         "solarflow 2400 ac": SolarFlow2400AC,
+        "solarflow 2400 ac+": SolarFlow2400AC_Plus,
         "superbase v6400": SuperBaseV6400,
         "superbase v4600": SuperBaseV4600,
         "solarflow 2400 pro": SolarFlow2400Pro,
